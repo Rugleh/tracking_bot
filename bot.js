20d2b41371ea265acf95911c9c02abf0b2b3c625
rugleh <diegorubilartagle@gmail.com>
2017-11-08 04:43:33 -0300

    make it better

---
+++ bot.js
@@ -0,0 +1,55 @@
+console.log('the reply bot is starting');
+
+var Twit = require('twit');
+var correos = require('correos-chile');
+var config = require('./config.js');
+var T = new Twit(config);
+
+var stream = T.stream('user');
+
+stream.on('tweet', tweetEvent);
+
+function tweetEvent(eventMsg) {
+
+    var replyto = eventMsg.in_reply_to_screen_name;
+    var text = eventMsg.text;
+    var from = eventMsg.user.screen_name;
+
+    console.log(replyto + ' ' + from);
+
+    array = text.split(' ');
+    codigo = array[1];
+
+    correos([codigo]).then(
+        r => {
+            estado1 = JSON.stringify(r[0].registros[0].estado).toLowerCase();
+            lugar1 = JSON.stringify(r[0].registros[0].lugar);
+            console.log(estado1);
+
+            if (replyto === 'Tracking_Bot') {
+                var newtweet = '@' + from + ' Tu paquete tiene este estado: ' + estado1 +" en "+ lugar1;
+                tweetIt(newtweet);
+            }
+
+        }).catch(
+        e => {
+            console.log(e);
+        })
+    
+}
+
+function tweetIt(txt) {
+    var tweet = {
+        status: txt
+    }
+
+    T.post('statuses/update', tweet, tweeted);
+
+    function tweeted(err, data, response){
+        if(err){
+            console.log("something went wrong");
+        } else {
+            console.log("it worked");
+        }
+    }
+}
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'HYuaOGSaDdlJutnWBUUeClAUJ',
+    consumer_secret: '2gbdDbD0QLJrkvC84hlzbG46C18WHfNe9IS6ZoZYsOK8CioI4x',
+    access_token: '928136809602830336-cNMocf9qnlhTqVPxguXFeXvVMAWsEiR',
+    access_token_secret: 'Mlr5hbo7cqRIVN0Na4Ggaty4jwD20PS7InYSpCVdwstFM'
+}
