618a6d718d930215c335fbbf9e4b128c679c3496
rugleh <diegorubilartagle@gmail.com>
2017-11-10 20:58:06 -0300

    random emoji

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ console.log('the reply bot is starting');
 
 var Twit = require('twit');
 var correos = require('correos-chile');
+var randomEmoji = require('random-emoji');
 var config = require('./config.js');
 var T = new Twit(config);
 
@@ -25,10 +26,11 @@ function tweetEvent(eventMsg) {
             estado1 = JSON.stringify(r[0].registros[0].estado).toLowerCase();
             lugar1 = JSON.stringify(r[0].registros[0].lugar);
             console.log(estado1);
-            randNum = Math.floor((Math.random() * 1000) + 1);
+            //randNum = Math.floor((Math.random() * 1000) + 1);
+            randEmoji = randomEmoji.random({ count: 2 });
 
             if (replyto === 'Tracking_Bot') {
-                var newtweet = '@' + from + ' Tu paquete tiene este estado: ' + estado1 + " en " + lugar1 + " | Tu número de la suerte 😀: " + randNum;
+                var newtweet = '@' + from + ' Estado de tu paquete: ' + estado1 + " en " + lugar1 + " | Tu número de la suerte 😀: " + randomEmoji[0].character + randomEmoji[1].character;
                 tweetIt(newtweet);
             }
 
